12e9bc5bf35d63791cebff621baa1adcc87878d2
cleitonfelipe <cleiton.felipe@yahoo.com.br>
2019-11-18 12:05:05 -0300

    Simplificando

--- Chat.py
+++ Chat.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-
-import speech_recognition as sr
-import wikipedia
-import pyttsx3
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot2 = ChatBot('Senior')
+bot = ChatBot('Senior')
 conv2 = ['oi',
          'ola',
          'ola, tudo bem?',
@@ -14,27 +10,14 @@ conv2 = ['oi',
          'qual o seu nome?',
          'meu nome é Chat, e o seu?']
 
-    bot2.set_trainer(ListTrainer)
-    bot2.train(conv2)
-    print('Chat: Olá o que quer saber?')
-    while True:
+bot.set_trainer(ListTrainer)
+bot.train(conv2)
 
-        audio = r.listen(s)
+while True:
+    quest = input('Voce: ')
+    resp = bot.get_response(quest)
 
-        speech = r.recognize_google(audio, language='pt-BR')
-        print('Eu:',speech)
-        res = evaluate(speech)
-
-        if res is not None:
-            ans = answer(speech)
-            print('Chat:',ans)
-            speak(ans)
-            print()
-
-        elif True:
-            resp = bot2.get_response(speech)
-            print('Chat:', resp)
-            speak(resp)
-        else:
-            print('Chat: Desculpe, não entendi!')
-            speak('Desculpe, não entendi!')
+    if float(resp.confidence) > 0.5:
+        print('Chat: ', resp)
+    else:
+        print('Chat: Desculpe, não entendi')
